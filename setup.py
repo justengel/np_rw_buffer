7c6c85556005a716db978a47c7428061181dfa88
justengel <jtengel08@gmail.com>
2020-02-17 17:08:36 -0500

    Made the C extension optional. (this should work?)

--- setup.py
+++ setup.py
@@ -27,6 +27,38 @@ def get_meta(filename):
         return meta
 
 
+# ========== Optional C extension ==========
+import logging
+from setuptools.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
+logging.basicConfig()
+log = logging.getLogger(__file__)
+
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError, IOError)
+
+
+class BuildFailed(Exception):
+    pass
+
+
+def construct_build_ext(build_ext):
+    class WrappedBuildExt(build_ext):
+        # This class allows C extension building to fail.
+        def run(self):
+            try:
+                build_ext.run(self)
+            except DistutilsPlatformError as x:
+                raise BuildFailed(x)
+
+        def build_extension(self, ext):
+            try:
+                build_ext.build_extension(self, ext)
+            except ext_errors as x:
+                raise BuildFailed(x)
+    return WrappedBuildExt
+
+
 if __name__ == "__main__":
     # Variables
     meta = get_meta('np_rw_buffer/__meta__.py')
@@ -47,48 +79,55 @@ if __name__ == "__main__":
                   sources=['src/circular_indexes.c'],
                   include_dirs=['src', numpy.get_include()]),
         ]
+    setup_kwargs = {'name': name,
+                    'version': version,
+                    'description': description,
+                    'long_description': read('README.rst'),
+                    'keywords': keywords,
+                    'url': url,
+                    'download_url': ''.join((url, '/archive/v', version, '.tar.gz')),
 
-    setup(name=name,
-          version=version,
-          description=description,
-          long_description=read('README.rst'),
-          keywords=keywords,
-          url=url,
-          download_url=''.join((url, '/archive/v', version, '.tar.gz')),
+                    'author': author,
+                    'author_email': author_email,
 
-          author=author,
-          author_email=author_email,
+                    'license': 'Proprietary',
+                    'platform': 'any',
+                    'classifiers': ['Programming Language :: Python',
+                                    'Programming Language :: Python :: 3',
+                                    'Operating System :: OS Independent'],
 
-          license='MIT',
-          platforms='any',
-          classifiers=['Programming Language :: Python',
-                       'Programming Language :: Python :: 3',
-                       'Operating System :: OS Independent'],
+                    'scripts': [file for file in glob.iglob('bin/*.py')],  # Run with python -m Scripts.module args
 
-          scripts=[file for file in glob.iglob('bin/*.py')],  # Run with python -m Scripts.module args
+                    # 'ext_modules': extensions,
+                    'packages': packages,
+                    'include_package_data': True,
+                    'package_data': {pkg: ['*', '*/*', '*/*/*', '*/*/*/*', '*/*/*/*/*']
+                                     for pkg in packages if '/' not in pkg and '\\' not in pkg},
 
-          ext_modules=extensions,  # C extensions
-          packages=packages,
-          include_package_data=True,
-          package_data={pkg: ['*', '*/*', '*/*/*', '*/*/*/*', '*/*/*/*/*']
-                        for pkg in packages if '/' not in pkg and '\\' not in pkg},
+                    'install_requires': [
+                        'numpy>=1.13.0',
+                        ],
+                    'extras_require': {
+                        },
+                    }
 
-          # Data files outside of packages
-          # data_files=[('my_data', ['data/my_data.dat'])],
+    # Comment this out if you want the simple setup
+    cmd_classes = setup_kwargs.setdefault('cmdclass', {})
+    setup_kwargs['cmdclass']['build_ext'] = construct_build_ext(build_ext)
 
-          # options to install extra requirements
-          install_requires=[
-              'numpy>=1.13.0',
-              ],
-          extras_require={
-              },
+    try:
+        # Run the setup with the c code
+        setup(ext_modules=extensions, **setup_kwargs)
+    except BuildFailed as err:
+        log.warning(err)
+        log.warning("The C extension could not be compiled")
 
-          # entry_points={
-          #     'console_scripts': [
-          #         'plot_csv=bin.plot_csv:plot_csv',
-          #         ],
-          #     'gui_scripts': [
-          #         'baz = my_package_gui:start_func',
-          #         ]
-          #     }
-          )
+        # Remove any previously defined build_ext command class.
+        if 'build_ext' in setup_kwargs['cmdclass']:
+            del setup_kwargs['cmdclass']['build_ext']
+        if 'build_ext' in cmd_classes:
+            del cmd_classes['build_ext']
+
+        # Run the setup without the c code
+        setup(**setup_kwargs)
+        log.info("Plain-Python installation succeeded.")
