064626a2883418223d2e55d9572ec600f1e8f7e1
justengel <jtengel08@gmail.com>
2020-02-17 16:51:14 -0500

    Updated to version 1.1.8

--- np_rw_buffer/__meta__.py
+++ np_rw_buffer/__meta__.py
@@ -1,5 +1,5 @@
 name = 'np_rw_buffer'
-version = '1.1.7'
+version = '1.1.8'
 description = 'Library to help store audio data in a circular buffer.'
 url = 'https://github.com/justengel/np_rw_buffer'
 author = 'Justin Engel'
